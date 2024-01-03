509ecee1f3aadc73528f9ddf6ff8010c1fa60ef9
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2024-01-03 23:46:03 +0530

    Revised linefollowing.ino

--- Task_4B/linefollowing/linefollowing.ino
+++ Task_4B/linefollowing/linefollowing.ino
@@ -1,36 +1,25 @@
 //Motor pin connections
 #define in1 16
 #define in2 4
-#define in3 2
-#define in4 15
+#define in4 2
+#define in3 15
 //ir pin connection
-#define irPin1 32
-#define irPin2 34
-#define irPin3 35
+#define irPin1 32 
+#define irPin2 35
+#define irPin3 34
 #define irPin4 36
 #define irPin5 39
 //buzzer and led connections
 #define buzzerPin 25
 #define ledPin 26
 int k=0;
-int l=0;
-void setup() {
-  //Motor pins as output
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  //sensor pin as input
-  pinMode(irPin1, INPUT);
-  pinMode(irPin2, INPUT);
-  pinMode(irPin3, INPUT);
-  pinMode(irPin4, INPUT);
-  pinMode(irPin5, INPUT);
-}
+int m=0;
+
 void moveForward(){
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
+  
   digitalWrite(in4, LOW);
 }
 void moveRight(){
@@ -97,10 +86,29 @@ void endNode(){
   digitalWrite(buzzerPin, LOW);
   digitalWrite(ledPin, LOW);
 }
+
+
+void setup() {
+  //Motor pins as output
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  //sensor pin as input
+  pinMode(irPin1, INPUT);
+  pinMode(irPin2, INPUT);
+  pinMode(irPin3, INPUT);
+  pinMode(irPin4, INPUT);
+  pinMode(irPin5, INPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
 void loop() {
-  if(l==0){
+  if(m==0){
     startNode();
-    l=1;
+    m=1;
   }
   if((irPin2) && !(irPin3)){
     digitalWrite(in1, LOW);
@@ -126,7 +134,7 @@ void loop() {
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
   } 
-  if(((irPin2) && (irPin3))||((irPin3) && (irPin4))){
+  else if(((irPin2) && (irPin3))||((irPin3) && (irPin4))){
     stopNode();
     leaveNode();
     k=k+1;
